6c3b2730e96ee790b7fe38d68c1dbccf518982b8
Nathan Lin <nlin@athenian.org>
2021-08-31 20:45:25 -0700

    did the homework

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,6 +5,16 @@ public class MyMain {
         Scanner scan = new Scanner(System.in);
 
         // Add some code here!
+        System.out.println("Hi!");
+        scan.nextInt();
+
+        Integer.parseInt(scan.next());
+
+        System.out.println("How are you doing mate?");
+
+        System.out.println("I like purple!");
+
+        System.out.println("What is the meaning of life?");
 
         scan.close();
     }
